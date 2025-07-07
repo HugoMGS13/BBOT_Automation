a5049dc6738f14e08cdfe5db05cbcbd1f8b8441f
Hugo <127580302+HugoMGS13@users.noreply.github.com>
2025-07-07 19:57:31 -0300

    Update scan_dataleak_treatment.py

--- profiles/scan_dataleak_treatment.py
+++ profiles/scan_dataleak_treatment.py
@@ -3,6 +3,12 @@ from scan_base import PerfilDeScanBase
 class PerfilDataLeak(PerfilDeScanBase):
     name = "scan_dataleak_treatment"
     description = "Scan de exploração de bases de dados buscando de vazamentos de senhas"
-    presets = ['subdomain-enum','spider']
-    modules = ["baddns","shodan_idb","shodan_dns",'pgp']
-    config = {'allow_deadly': True}
+    presets = ['']
+    modules = ["baddns","dehashed",'pgp']
+    config = {{
+        "modules": {
+            "dehashed": {
+                "api_key": "1SS44Jqy/hZD5IfQgDZ9HX4VkCJiM3f2b/QUt6xFreRN2LHNmpVGOMs="
+            }
+        }
+    }}
